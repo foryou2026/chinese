d72ff1996cb480b193a3df9f4650bf410866b141
wangqinglong2024 <2277954757@qq.com>
2026-05-31 16:08:23 +0800

    1.2.6 电商四阶段md delete i18n

--- template/i18n/system/packages/shared-i18n/src/index.ts
+++
@@ -1,8 +0,0 @@
-import zh from './zh.ts';
-import en from './en.ts';
-import vi from './vi.ts';
-import th from './th.ts';
-import id from './id.ts';
-
-export const RESOURCES = { zh, en, vi, th, id } as const;
-export type ResourceShape = typeof en;
